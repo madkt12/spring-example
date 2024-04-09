f2123c498f4c2e58936dcb1a3ed28d40c37b778f
mahdad_12@yahoo.com <mahdad_12@yahoo.com>
2024-04-09 21:57:09 +0300

    test

--- src/test/java/sda/remote28/springexample/SpringExampleApplicationTests.java
+++ src/test/java/sda/remote28/springexample/SpringExampleApplicationTests.java
@@ -1,13 +1,21 @@
 package sda.remote28.springexample;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.PropertySource;
 
+@PropertySource("classpath:jenkins.properties")
 @SpringBootTest
 class SpringExampleApplicationTests {
 
+	@Value("${messageFromJenkins}")
+	private String message;
+
 	@Test
-	void contextLoads() {
+	void testValueWithJenkinsParametrisedBuild() {
+		System.out.println("Test Started with value: " + message);
+		assert message.equalsIgnoreCase("Java");
 	}
 
 }
