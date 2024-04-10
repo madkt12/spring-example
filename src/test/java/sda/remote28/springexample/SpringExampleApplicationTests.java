72d46023e95924e942522c62584f3e2ffa00c062
mahdad_12@yahoo.com <mahdad_12@yahoo.com>
2024-04-10 10:25:02 +0300

    test

--- src/test/java/sda/remote28/springexample/SpringExampleApplicationTests.java
+++ src/test/java/sda/remote28/springexample/SpringExampleApplicationTests.java
@@ -3,8 +3,10 @@ package sda.remote28.springexample;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(locations = "classpath:jenkins_temp.properties")
 class SpringExampleApplicationTests {
 
 	@Value("${messageFromJenkins}")
